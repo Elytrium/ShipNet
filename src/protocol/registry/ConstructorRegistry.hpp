d02d1049481806a288cfff80753a0b89be2e7e30
Petr Ilin <hevav@hevav.dev>
2023-07-14 09:29:56 +0300

    Errorable support in ConstructorRegistry

--- src/protocol/registry/ConstructorRegistry.hpp
+++ src/protocol/registry/ConstructorRegistry.hpp
@@ -7,13 +7,6 @@
 namespace Ship {
 
   template<typename T>
-  inline std::function<T*(const ProtocolVersion* version, ByteBuffer* buffer)> CreateConstructor() {
-    return [=](const ProtocolVersion* version, ByteBuffer* buffer) {
-      return new T(version, buffer);
-    };
-  }
-
-  template<typename T>
   class NoConstructorExistErrorable : public Errorable<T> {
    public:
     static inline const uint32_t TYPE_ORDINAL = OrdinalRegistry::ErrorableTypeRegistry.RegisterOrdinal();
@@ -40,26 +33,54 @@ namespace Ship {
   };
 
   template<typename T>
+  class InvalidConstructorErrorable : public Errorable<T> {
+   public:
+    static inline const uint32_t TYPE_ORDINAL = OrdinalRegistry::ErrorableTypeRegistry.RegisterOrdinal();
+
+    explicit InvalidConstructorErrorable() : Errorable<T>(TYPE_ORDINAL, {}, 0) {
+    }
+
+    void Print(std::ostream o) {
+      o << "Invalid constructor for " << typeid(T).name();
+    }
+  };
+
+  template<typename T, typename R>
+  inline std::function<Errorable<T*>(const ProtocolVersion* version, ByteBuffer* buffer)> CreateConstructor() {
+    return [=](const ProtocolVersion* version, ByteBuffer* buffer) {
+      return SuccessErrorable<T*>((T*) new R(version, buffer));
+    };
+  }
+
+  template<typename T, typename R>
+  inline std::function<Errorable<T*>(const ProtocolVersion* version, ByteBuffer* buffer)> WrapConstructor() {
+    return [=](const ProtocolVersion* version, ByteBuffer* buffer) {
+      ProceedErrorable(object, R, R::Instantiate(version, buffer), (Errorable<T*>) InvalidConstructorErrorable<T*>())
+      return (Errorable<T*>) SuccessErrorable<T*>((T*) &object);
+    };
+  }
+
+  template<typename T>
   class ConstructorRegistry : public VersionedRegistry {
    private:
-    std::vector<std::function<T*(const ProtocolVersion* version, ByteBuffer* buffer)>> ordinalToObjectMap;
+    std::vector<std::function<Errorable<T*>(const ProtocolVersion* version, ByteBuffer* buffer)>> ordinalToObjectMap;
 
    public:
     explicit ConstructorRegistry(const ProtocolVersions& versions, const std::set<ProtocolVersion>& versionMap) : VersionedRegistry(versions, versionMap) {}
 
-    void RegisterConstructor(uint32_t ordinal, const std::function<T*(const ProtocolVersion* version, ByteBuffer* buffer)>& constructor) {
+    void RegisterConstructor(uint32_t ordinal, const std::function<Errorable<T*>(const ProtocolVersion* version, ByteBuffer* buffer)>& constructor) {
       OrdinalVector::ResizeVectorAndSet(ordinalToObjectMap, ordinal, constructor);
     }
 
     Errorable<T*> GetObjectByID(const ProtocolVersion* version, uint32_t id, ByteBuffer* buffer) const {
       ProceedErrorable(ordinal, uint32_t, GetOrdinalByID(version, id), NoConstructorOrdinalExistErrorable<T*>(id))
-        if (ordinal >= ordinalToObjectMap.size()) {
+      if (ordinal >= ordinalToObjectMap.size()) {
         return NoConstructorExistErrorable<T*>(ordinal);
       }
 
-      std::function<T*(const ProtocolVersion* version, ByteBuffer* buffer)> constructor = ordinalToObjectMap[ordinal];
+      std::function<Errorable<T*>(const ProtocolVersion* version, ByteBuffer* buffer)> constructor = ordinalToObjectMap[ordinal];
       if (constructor) {
-        return SuccessErrorable<T*>(constructor(version, buffer));
+        return constructor(version, buffer);
       } else {
         return NoConstructorExistErrorable<T*>(ordinal);
       }
